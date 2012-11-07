fad64e27eb65e0253bfb01ac9e0449f5a88d0e86
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2012-11-07 13:53:32 +0100

    x509-noop.c: initial checkin

---
+++ x509-noop.c
@@ -0,0 +1,26 @@
+/*	--*- c -*--
+ * Copyright (C) 2012 Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include "signature.h"
+
+struct signature_algorithm *	signature_algorithm_x509_create(void)
+{
+	return NULL;
+}
